d624289a6ee20c81ea6b402934fe1c47303c52d7
Aryan Gupta <aryang23936@gmail.com>
2024-05-14 16:18:02 +0530

    Sending API json response for all courses

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 // Model for course - file
@@ -27,4 +29,18 @@ func (c *Course) IsEmpty() bool {
 
 func main() {
 	fmt.Println("API - LearnCodeOnline.in")
+}
+
+//controllers - file
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API by LearnCodeOnline</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "applicatioan/json")
+	json.NewEncoder(w).Encode(courses)
 }
